43a0b14efea12ed852d4a63f6f217e41595b360f
mdfarhanrafi <mdfarhanrafi563@gmail.com>
2025-02-02 22:19:23 +0600

    themestore

--- frontend/store/useThemeStore.js
+++ frontend/store/useThemeStore.js
@@ -1,10 +1,9 @@
-// 'use client'
-// import { create } from "zustand";
+'use client'
+import { create } from "zustand";
 
-// export const useThemeStore = create((set) => ({
-//   theme: localStorage.getItem("chat-theme") || "coffee",
-//   setTheme: (theme) => {
-//     localStorage.setItem("chat-theme", theme);
-//     set({ theme });
-//   },
-// }));
+export const useThemeStore = create((set) => ({
+  theme: "coffee",
+  setTheme: (theme) => {
+    set({ theme });
+  },
+}));
