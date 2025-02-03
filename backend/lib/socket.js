2c8fd1b59ae14568029a9b1e03436923121abc3a
mdfarhanrafi <mdfarhanrafi563@gmail.com>
2025-02-03 12:26:54 +0600

    backend socket and server cors

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -8,31 +8,35 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["https://chat-app-blush-phi.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
-export function getReceiverSocketId(userId) {
-    return userSocketMap[userId];
+
+// Online users storage
+const userSocketMap = {}; // { userId: socketId }
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+    socket.userId = userId; // Store userId inside socket object
   }
-  
-  // used to store online users
-  const userSocketMap = {}; // {userId: socketId}
-  
-  io.on("connection", (socket) => {
-    console.log("A user connected", socket.id);
-  
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
-  
-    // io.emit() is used to send events to all the connected clients
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    if (socket.userId) delete userSocketMap[socket.userId]; // Use stored userId
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-  
-    socket.on("disconnect", () => {
-      console.log("A user disconnected", socket.id);
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
   });
+});
 
-
+// Function to get socket ID of a receiver
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
 
 export { io, app, server };
